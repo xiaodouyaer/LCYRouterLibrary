522b4d7342cda6e65af39aae38272558c35feacc
XZ-201803231121\Administrator <1090969255@qq.com>
2018-05-21 08:57:41 +0800

    先初始一个能跑的例子，后续需要重做commonutils库，完善main组件

---
+++ app.main/src/main/java/com/liuchaoya/main/activitys/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.liuchaoya.main.activitys
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.liuchaoya.main.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app.main/src/main/java/com/liuchaoya/main/debug/MyApplication.java
@@ -0,0 +1,13 @@
+package com.liuchaoya.main.debug;
+
+import android.app.Application;
+
+/**
+ * @classDescription: 描述：
+ * @author: LiuChaoya
+ * @createTime: 2018/5/18 0018 16:11.
+ * @email: 1090969255@qq.com
+ */
+public class MyApplication extends Application {
+    
+}
